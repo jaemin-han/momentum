96852aeb9a0ac54956782a38aecd27fa7ff988df
Jaemin Han <jaemin.han@nyu.edu>
2017-01-04 00:18:13 -0500

    momentum initial setup server

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(port, () => console.log('Server in 2017', port));
